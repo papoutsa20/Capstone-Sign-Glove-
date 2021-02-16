68023fc9d06663f6f8940ba98f2ea119fabe6036
Jason-Twigg <twigg20@up.edu>
2021-02-16 13:32:07 -0800

    Adding predict code and other modifications.

--- train/predict.py
+++ train/predict.py
@@ -1,3 +1,6 @@
+from train import LETTERS
+import numpy as np
+
 def predictValues(model):
     while True:
         inputStr = input("Enter an input string: {d1, d2, d3, d4, d5, gx, gy, gz}\n" +
@@ -5,19 +8,21 @@ def predictValues(model):
         if inputStr == "q" or inputStr == "quit": return 0
         
         inputs = inputStr.strip("{}()[]").split(',')
-
-        print(inputs)
+        inputs = [float(i) for i in inputs]
+        inputs = np.array([inputs], dtype=float)
+        print(inputs)        
         modelGuess = predictValue(inputs, model)
-        print(modelGuess)
+        #print(modelGuess)
+        printPrediction(modelGuess)
         print("\n")
     return 1 
 
 #Takes input (d1, d2, d3, d4, d5, gx, gy, gz) as input
 def predictValue(inputs, model):
-    if type(inputs) is not list:
+    if type(inputs) is not list and False:
         print("Error: Input (inputs) for predictValue is not a list")
         return 0
-    elif len(inputs) != 8:
+    if len(inputs) != 8 and False:
         print(f"Error: Input (inputs) for predictValues has incorrect length.\n" +
               "(8) expected and ({}) given.".format(len(inputs)))
         return 0
@@ -26,16 +31,24 @@ def predictValue(inputs, model):
         print("Warning: Model given was invalid, printing default output instead")
         guessWeights = [0.0] * 26;
     else:
-        guessWeights = model.predict(inputs)[0][:]
+        guessWeights = model.predict(inputs)[0]
     return guessWeights
 
 def printPrediction(guessWeights):
-    if type(guessWeights) is not list:
+    if type(guessWeights) is not list and False:
         print("Error: Input (inputs) for printPrediction is not a list")
         return 0
-    elif len(guessWeights) != 26:
+    elif len(guessWeights) != 26 and False:
         print(f"Error: Input (guessWeights) for printPrediction has incorrect length.\n" +
               "(26) expected and ({}) given.".format(len(inputs)))
         return 0
+    str = '' 
+    max_index = np.argmax(guessWeights)
+    print("I believe that is a {}".format(LETTERS[max_index]))
+    for i in range(26):
 
-predictValues(None)
+        str += LETTERS[i]
+        str += ' '
+        str += "{:10.4f}".format(float(guessWeights[i]))
+        str += '\n'
+    print(str)
--- train/train.py
+++ train/train.py
@@ -1,7 +1,8 @@
-#import tensorflow as tf
+import tensorflow as tf
 import numpy as np
 import os
-#from sklearn.utils import shuffle
+from sklearn.utils import shuffle
+import predict
 DATA_PATH = os.path.join(os.path.dirname(__file__), 'data')
 LETTERS = (
         'A',
@@ -56,17 +57,39 @@ def preprocess_data(data):
     max_value = 1023.0
     return data / 1023.0 
 
+def create_model():
+
+    model = tf.keras.Sequential([
+        tf.keras.layers.Dense(10, activation='relu', input_shape=(8,)),
+        tf.keras.layers.Dense(30, activation='relu'),
+        tf.keras.layers.Dense(26, activation='softmax')
+    ])
+
+    model.compile(optimizer='adam',
+        loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+        metrics=['accuracy'])
+    return model
 
 def split_train_test(data, labels, percent_train=.80):
     cut_off = int(data.shape[0]*.80)
-    train_x = all_grades[:cut_off]
-    train_y = all_labels[:cut_off]
-    test_x = all_grades[cut_off:]
-    test_y = all_labels[cut_off:]
+    grades, labels = shuffle(data, labels)
+    train_x = grades[:cut_off]
+    train_y = labels[:cut_off]
+    test_x = grades[cut_off:]
+    test_y = labels[cut_off:]
     return train_x, train_y, test_x, test_y
 
 if '__main__' == __name__:
-   print('reading data from {}'.format(DATA_PATH))
-   data, labels = read_data() 
-   print(data)
-   print(labels)
+    print('reading data from {}'.format(DATA_PATH))
+    data, labels = read_data() 
+    print(data)
+    print(labels)
+
+    model = create_model()
+    train_x, train_y, test_x, test_y = split_train_test(data, labels)
+
+    model.fit(train_x, train_y, epochs=100)
+
+    model.evaluate(test_x, test_y, verbose=2)
+
+    predict.predictValues(model)
